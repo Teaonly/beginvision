a455eb512ce179713a48ef1e08e28e500abae4e5
Zhou,Chang <achang.zhou@gmail.com>
2013-07-17 00:56:37 +0800

    Tested histogram module Signed-off-by: Zhou,Chang <achang.zhou@gmail.com>

--- examples/build.sh
+++ examples/build.sh
@@ -1,2 +1,1 @@
-g++ -o ex1 example1.cpp -I../ -I../third_party
-g++ -o ex2 example2.cpp -I../ -I../third_party
+g++ -o ex3 example3.cpp -I../ -I../third_party
---
+++ examples/example3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#include <bv/image.h>
+#include <bv/image_convert.h>
+#include <bv/histogram.h>
+
+int main(int argc, char *argv[]) {
+    if ( argc < 2) {
+        std::cout << "Please input bmp file!" << std::endl;
+        return -1;
+    }
+    
+    bv::ColorImage<3> colorImage( argv[1] );
+    bv::Image img( colorImage.color(0).width(), colorImage.color(0).height() );
+    
+    bv::Convert::colorImageToGrayImage(colorImage, img);
+    bv::Histogram hist(img);
+    hist.equaliseMatrix(img);
+
+    bv::Convert::grayImageToColorImage(img, colorImage);
+    colorImage.SaveImageToBMP("/tmp/xx.bmp");
+}
