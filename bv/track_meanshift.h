743dbf7d0c58444e5b0d132312fa09c2764b9632
Zhou Chang <achang.zhou@gmail.com>
2013-09-06 18:37:42 +0800

    Prepared for Mean shift tracker algorithm. Signed-off-by: Zhou Chang <achang.zhou@gmail.com>

---
+++ bv/track_meanshift.h
@@ -0,0 +1,18 @@
+#ifndef _BV_TRACK_MEANSHIFT_H_
+#define _BV_TRACK_MEANSHIFT_H_ 
+
+#include <vector>
+#include <Eigen/Core>
+#include <Eigen/LU> 
+#include <cmath>
+#include "beginvision.h"
+#include "filter.h"
+#include "util.h"
+
+namespace bv {
+
+
+
+}
+#endif
+
