c9a77f598785f16fcbc53a58407fd6f418898214
Zhou Chang <achang.zhou@gmail.com>
2013-10-08 12:54:36 +0800

    Adjust image scale-space building.

--- bv/detector_sift.h
+++ bv/detector_sift.h
@@ -16,9 +16,10 @@ namespace bv {
 
 class DT_Sift {
 public:
-    DT_Sift(int numOctaves = 4, int numLevels = 5, int minLevel = 0, int minOctave = 0, int S = 3): 
-            numOctaves_(numOctaves), numLevels_(numLevels), minLevel_(minLevel), minOctave_(minOctave), S_(S) { 
-
+    DT_Sift(int numOctaves = 4, int S = 3, int minOctave = 0): 
+            numOctaves_(numOctaves), minOctave_(minOctave), S_(S) { 
+        
+        numLevels_ = S_ + 3;
         k = powf(2, 1/S_);
         sigmaNominal_ = 0.5;
         sigma0_ = 1.6; 
@@ -32,8 +33,11 @@ public:
         I = I - Eigen::MatrixXd::Ones(img.rows(), img.cols()) * I.minCoeff();
         I = I / I.maxCoeff();    
             
-        // 1. upsamle/downsample the original image, TODO
-        
+        // 1. upsamle/downsample the original image
+        if ( minOctave_ != 0) {
+            // TODO 
+        }     
+    
 
         // 2. building scale-space image
         buildOctaves(I); 
@@ -54,11 +58,13 @@ private:
             sqrt( sigma(o,s+1)^2 - sigma(o,s)^2) =
        =    sqrt( sigma0^2 * 2^(2o) * ( 2^((2s+2)/S) - 2^(2s/S) ))
        =    sigma0 * 2^o * sqrt ( 2^(2s/S) ( 2^(2/S) - 1) )
-       =    sigma0 * 2^(o+s/S) * sqrt( 2^(2/S) - 1)         
+       =    sigma0 * 2^(o+s/S) * sqrt( 2^(2/S) - 1)
+
+       we can ommit the 2^o in the same octvae.  
     */
     void buildOctaves( Eigen::MatrixXd& I) {
         Eigen::MatrixXd bottomLevel = I;
-        double bottomSigma = sigma0_ * powf(2, minOctave_ + 1.0 * minLevel_ / S_);
+        double bottomSigma = sigma0_ * powf(2, minOctave_);
 
         if ( bottomSigma > sigmaNominal_ * powf(2, minOctave_)) {
             double sa = bottomSigma;
@@ -66,12 +72,13 @@ private:
             double sigma = sqrt( sa*sa - sb*sb);
             
             Eigen::MatrixXd temp = bottomLevel;
-            std::cout << " sigma = " << sigma << std::endl;
+            std::cout << " bottom sigma = " << bottomSigma << std::endl;
+            std::cout << " first  sigma = " << sigma << std::endl;
             Filter::gaussianBlur(bottomLevel, temp, (int)(sigma*3+0.5)*2 + 1, sigma);
             bottomLevel = temp;
         }
 
-        for ( int oi = 0; oi < numOctaves_; oi++) {
+        for(int oi = 0; oi < 1; oi++) {
             int currentOctave = oi + minOctave_;
         
             SiftImageOctave octave;
@@ -80,8 +87,9 @@ private:
             octave.images_.push_back( std::pair<double, Eigen::MatrixXd> (bottomSigma, bottomLevel) );
             double lastSigma = bottomSigma;
             Eigen::MatrixXd lastLevel = bottomLevel;
+            
             for ( int li = 1; li < numLevels_; li++) {
-                double currentSigma = powf(2, currentOctave + (minLevel_ + li) / S_);
+                double currentSigma = sigma0_ * powf(2, li * 1.0 / S_);
                 double diffSigma = sqrt( currentSigma*currentSigma - lastSigma*lastSigma);
                 lastSigma = currentSigma;
 
@@ -93,6 +101,7 @@ private:
                 octave.images_.push_back( std::pair<double, Eigen::MatrixXd> (currentSigma, temp) );
                 lastLevel = temp;
             }
+             
         }    
     
     
