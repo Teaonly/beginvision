7e905f56e4dee654172cc4ef453b3742b8b93b5c
Zhou Chang <achang.zhou@gmail.com>
2013-10-21 14:52:09 +0800

    Just Updated. Signed-off-by: Zhou Chang <achang.zhou@gmail.com>

--- bv/descriptor_sift.h
+++ bv/descriptor_sift.h
@@ -20,6 +20,7 @@ class DS_Sift {
 public:    
     DS_Sift(DT_Sift& dt):detector_(dt) {
         NBP_ = 4;
+        NBO_ = 8;
         winFactor_ = 3.0;
         run();
     }
@@ -27,6 +28,7 @@ public:
 private:
     void run() {
         std::vector<DT_Sift::SiftImageOctave>& octaves(detector_.octaves_);
+        double weightSigma = NBP_ / 2.0;
         
         for(int i = 0; i < detector_.keyPoints_.size(); i++) {  
             DT_Sift::SiftKeyPoint n = detector_.keyPoints_[i]; 
@@ -43,10 +45,9 @@ private:
 
             int width = octaves[oi].images_[si].rows();
             int height = octaves[oi].images_[si].cols();
-            double weightSigma = winFactor_ * (NBP_/2) * detector_.sigma0_ * powf(2, s/detector_.S_ );
-            int windowSize = floor( weightSigma * sqrt(2) + 0.5);
- 
-                       
+            double binSize = winFactor_ * detector_.sigma0_ * powf(2, s/detector_.S_ );
+            int windowSize = floor( binSize * sqrt(2) * (NBP_+1) / 2.0 + 0.5);
+            
             // Define the are of descriptor's window
             int leftX = Util::max(cx-windowSize, 0);
             int rightX = Util::min(cx+windowSize, width-1); 
@@ -54,11 +55,26 @@ private:
             int bottomY = Util::min(cy+windowSize, height-1);
             for (int x = leftX; x <= rightX; x++) {
                 for ( int y = topY; y <= bottomY; y++) {
+
                     double xx = x - n.xx_;
                     double yy = y - n.yy_;
-                    double dx = cos(mainAngle) * xx + sin(mainAngle) * yy;
-                    double dy = -1*sin(mainAngle) * xx + cos(mainAngle) * yy;     
-                      
+                    double dx = (cos(mainAngle) * xx + sin(mainAngle) * yy) / binSize;
+                    double dy = (-1*sin(mainAngle) * xx + cos(mainAngle) * yy) / binSize;
+                    double weight = exp( (dx*dx+dy*dy) / (2.0*weightSigma*weightSigma) );
+                    double mag =   octaves[oi].gradsX_[si](x,y) * octaves[oi].gradsX_[si](x,y) 
+                                 + octaves[oi].gradsY_[si](x,y) * octaves[oi].gradsY_[si](x,y);
+                    mag = sqrt(mag);
+                    double angle = atan2( octaves[oi].gradsY_[si](x,y), octaves[oi].gradsX_[si](x,y) );
+                    if ( angle < 0) {  
+                        angle = angle + 2*PI;
+                    }
+                    angle = 1.0 * NBO_ * angle / (2.0*PI) ; 
+                    int xbin = floor(dx - 0.5);
+                    int ybin = floor(dy - 0.5);
+                    int abin = floor(angle);  
+                    
+                    //
+
                 }
             }
         }
@@ -66,12 +82,12 @@ private:
     
 private:
     typedef struct {
-                
     } SiftDescriptor; 
 
     DT_Sift& detector_;   
     double winFactor_; 
     int NBP_;
+    int NBO_;
 
     std::vector<SiftDescriptor> descs_;
 };
